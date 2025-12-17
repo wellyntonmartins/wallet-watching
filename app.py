177fba5348eb9f230a1d68880503b0401a8f8a1b
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2025-12-16 21:12:21 -0300

    Update 16/12/2025 (First Impression Nav's)

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
 
