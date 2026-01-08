aedf6b87948a6844ed1d95d85dfc4d27258553b9
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-01-08 19:45:07 -0300

    Update 08/01/2026 (Deploy - Part 6)

--- connection.py
+++ connection.py
@@ -2,10 +2,13 @@ import os
 import mysql.connector
 
 def get_db_connection():
+    host = os.getenv("MYSQLHOST")
+    if not host:
+        raise ValueError("MYSQLHOST environment variable not set!")
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQDATABASE"),
-        port=int(os.getenv("MYSQLPORT", 38454))
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
