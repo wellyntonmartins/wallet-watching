7bee8a2d2f6642e3ebe3adf1999b10dea1100d9f
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-01-08 20:17:33 -0300

    Update 08/01/2026 (Deploy - Part 8)

--- connection.py
+++ connection.py
@@ -6,6 +6,6 @@ def get_db_connection():
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQDATABASE"),
+        database=os.getenv("MYSQLDATABASE"),
         port=int(os.getenv("MYSQLPORT", 3306))
     )
