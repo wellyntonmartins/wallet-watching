6c1e4c3076cd3aa3ae031c8ec8d7b8e57473fb55
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-01-08 17:44:41 -0300

    Update 08/01/2026 (Deploy flask app to railway - Part 2)

---
+++ connection.py
@@ -0,0 +1,11 @@
+import os
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT", 3306))
+    )
